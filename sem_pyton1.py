d4018854d0e0a6047c28dab768076fb35dee7522
Ivan Chevin <chevin.gm@gmail.com>
2022-11-07 21:14:15 +0300

    семинар 1 07.11.2022

---
+++ sem_pyton1.py
@@ -0,0 +1,24 @@
+# a=3
+# b=3.4
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a**b)
+# print(a//b)
+# print(a%b)
+
+# Задача 1
+# Напишите программу, которая принимает на вход два 
+# числа и проверяет, является ли одно число квадратом другого.
+# Да. Нет
+
+a=int(input("a = "))
+b=int(input("a = "))
+if a*a==b:
+    print("да")
+elif b*b==a:
+    print("да")
+else:
+    print("нет")
+
