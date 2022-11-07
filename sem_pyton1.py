f3c1e81fecdd3842c628941ae863ffbf801d3765
Ivan Chevin <chevin.gm@gmail.com>
2022-11-07 21:16:25 +0300

    семинар 1 07.11.2022

--- sem_pyton1.py
+++ sem_pyton1.py
@@ -13,12 +13,14 @@
 # числа и проверяет, является ли одно число квадратом другого.
 # Да. Нет
 
-a=int(input("a = "))
-b=int(input("a = "))
-if a*a==b:
-    print("да")
-elif b*b==a:
-    print("да")
-else:
-    print("нет")
+# a=int(input("a = "))
+# b=int(input("a = "))
+# if a*a==b:
+#     print("да")
+# elif b*b==a:
+#     print("да")
+# else:
+#     print("нет")
+
+
 
