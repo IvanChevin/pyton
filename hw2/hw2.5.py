d9e5f1e443f40a18b620912137e1eaafe91ce8c0
Ivan Chevin <chevin.gm@gmail.com>
2022-11-11 01:07:00 +0300

    hw 3 задача 1, 2

--- hw2/hw2.5.py
+++ hw2/hw2.5.py
@@ -7,18 +7,27 @@
 # Output format
 # Целые числе через пробел
 
-n = list(map(int, input().split()))
-temp = 0
-for i in range(len(n)):
-    if i % 2 == 0 and i != 0:
-        # temp = n[i]
-        # n[i] = n[i - 1]
-        # n[i - 1] = temp
-        n[i], n[i-1] = n[i-1], n[i]
-    elif i + 1 < len(n):
-        # temp = n[i]
-        # n[i] = n[i + 1]
-        # n[i + 1] = temp
-        n[i], n[i+1] = n[i+1], n[i]
-    print(n)
-print(*n)
+# n = list(map(int, input().split()))
+# temp = 0
+# for i in range(len(n)):
+#     if i % 2 == 0 and i > 0:
+#         # temp = n[i]
+#         # n[i] = n[i - 1]
+#         # n[i - 1] = temp
+#         n[i], n[i-1] = n[i-1], n[i]
+#     elif i % 2 != 0 and i + 1 < len(n):
+#         # temp = n[i]
+#         # n[i] = n[i + 1]
+#         # n[i + 1] = temp
+#         n[i], n[i+1] = n[i+1], n[i]
+#     print(n)
+# print(*n)
+
+i = [int(x) for x in input().split()]
+for j, item in enumerate(i):
+    if item % 2 == 0 and j > 0:
+        i[j], i[j-1] = i[j-1], i[j]
+    elif item % 2 != 0 and j < len(i)-1:
+        i[j], i[j+1] = i[j+1], i[j]
+    print(i)
+print(*i, sep=" ", end="")
