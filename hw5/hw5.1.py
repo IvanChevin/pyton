9b7b4035f1e9d86d787c2f20ccd4820a33f56d17
Ivan Chevin <chevin.gm@gmail.com>
2022-11-19 07:44:40 +0300

    sem 7

--- hw5/hw5.1.py
+++ hw5/hw5.1.py
@@ -4,5 +4,9 @@ text_str = 'абв парампампам абвегд прампампам'
 text_list = text_str.split()
 for i in range(len(text_list)-2):
     if 'абв' in text_list[i]:
-        del text_list[i]
+        if not text_list[i].isidentifier():
+            text_list[i] = text_list[i][-1]
+        # del text_list[i]
+        else:
+            text_list[i] = ''
 print(*text_list)
--- hw5/hw5.2.py
+++ hw5/hw5.2.py
@@ -1,8 +1,8 @@
 # Создайте программу для игры с конфетами человек против человека.
 
-# Условие задачи: На столе лежит 2021 конфета. Играют два игрока делая ход друг после друга. 
-# Первый ход определяется жеребьёвкой. За один ход можно забрать не более чем 28 конфет. 
-# Все конфеты оппонента достаются сделавшему последний ход. 
+# Условие задачи: На столе лежит 2021 конфета. Играют два игрока делая ход друг после друга.
+# Первый ход определяется жеребьёвкой. За один ход можно забрать не более чем 28 конфет.
+# Все конфеты оппонента достаются сделавшему последний ход.
 # Сколько конфет нужно взять первому игроку, чтобы забрать все конфеты у своего конкурента?
 
 # a) Добавьте игру против бота
@@ -10,45 +10,80 @@
 # b) Подумайте как наделить бота ""интеллектом""
 
 import random
-import os
-
-candies_on_table = 100
-candies_for_step = 28
 
 def pvp():
-    count = 0
-    while candies_on_table > 0:
-        if count == 0:
-            step = int(input('Ходит Игрок 1. Введи количество конфет: '))
-            if step > candies_for_step or step > candies_on_table:
-                step = int(input('Слишком много! Возьми меньше: '))
-            candies_on_table -= step
-        if candies_on_table > 0:
-            print(f'на столе еще {candies_on_table} конфет!')
-            count = 1
-        else:
-            print('Конфеты закончились! Победил Игрок 1!')
-            
-        if count == 1:
-            step = int(input('Ходит Игрок 2. Введи количество конфет: '))
-            if step > candies_for_step or step > candies_on_table:
-                step = int(input('Слишком много! Возьми меньше: '))
-            candies_on_table -= step
-        if candies_on_table > 0:
-            print(f'на столе еще {candies_on_table} конфет!')
-            count = 0
-        else:
-            print('Конфеты закончились! Победил Игрок 2!')
+  candies_on_table = 2021
+  candies_for_step = 28
+  count = 0
+  while candies_on_table > 0:
+    if count == 0:
+      step = int(input('Ходит Игрок 1. Введи количество конфет: '))
+      if step > candies_for_step or step > candies_on_table:
+        step = int(input('Слишком много! Возьми меньше: '))
+      candies_on_table = candies_on_table - step
+    if candies_on_table > 0:
+      print(f'на столе еще {candies_on_table} конфет!')
+      count = 1
+    else:
+      print('Конфеты закончились! Победил Игрок 1!')
 
-# def pve():
+    if count == 1:
+      step = int(input('Ходит Игрок 2. Введи количество конфет: '))
+      if step > candies_for_step or step > candies_on_table:
+        step = int(input('Слишком много! Возьми меньше: '))
+      candies_on_table -= step
+    if candies_on_table > 0:
+      print(f'на столе еще {candies_on_table} конфет!')
+      count = 0
+    else:
+      print('Конфеты закончились! Победил Игрок 2!')
+  
+def pve():
+  candies_on_table = 2021
+  candies_for_step = 28
+  players = ['Игрок','Компьютер']
+  mover = random.randint(-1, 0)
+  print(f'Первым ходит {players[mover+1]}!')
 
+  while candies_on_table > 0:
+    mover += 1
 
+    if players[mover % 2] == 'Компьютер':
+      if candies_on_table < 29:
+        step = candies_on_table
+      else:
+        step = candies_on_table % (candies_for_step + 1)
+        if step == 0:
+          step = random.randint(1, candies_for_step)
+        # delenie = candies_on_table // candies_for_step
+        # step = candies_on_table - (delenie * candies_for_step + 1)
+      print(f'Компьютер забирает {step} конфет')
+
+    else:
+      step = 0
+      while step < 1 or step > 28:
+        step = int(input('Введи количество конфет: '))
+        if step > candies_for_step or step > candies_on_table:
+          step = int(input('Слишком много! Возьми меньше: '))
+        if step == 0:
+          step = int(input('Ты должен взять хотябы одну конфетку: '))
+        
+    candies_on_table = candies_on_table - step
+    if candies_on_table > 0:
+      print(f'на столе еще {candies_on_table} конфет!')
+    else:
+      print(f'Конфеты закончились! Победил {players[mover % 2]}!')
+  
 print("""1. Игрок против игорока
 2. Игрок против компьютера""")
 mode = 0
-while mode != 1 or mode !=2:
-    if mode == 1:
-        pvp()
-        os.abort()
-    else: 
-        mode = int(input('Введите 1 или 2 чтобы выбрать режим: '))
+while mode != 1 or mode != 2:
+  if mode == 1:
+    print(pvp())
+    exit()
+  elif mode == 2:
+    print(pve())
+    exit()
+  else:
+    mode = int(input('Введите 1 или 2 чтобы выбрать режим: '))
+
